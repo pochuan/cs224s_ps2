66d5e48bb6b24bc1f3edaac1819844f6809eeba0
pochuan <pochuan@stanford.edu>
2014-04-13 01:24:26 -0700

    Initialize trellis

--- viterbi.py
+++ viterbi.py
@@ -1,12 +1,14 @@
 import re
+from collections import OrderedDict
+
 print "Viterbi!"
 
 # Read in files
 lexicon = {}
 with open('lexicon.txt') as f:
 	for line in f:
-		split = line.split(' ', 1)
-		lexicon[split[0]] = split[1].strip()
+		split = re.split(r'\s+', line)
+		lexicon[split[0]] = split[1:-2]
 
 phonemes = []
 with open('phonemes.txt') as f:
@@ -24,6 +26,23 @@ with open('output.txt') as f:
 			B[t][split[1]] = {}
 		B[t][split[1]][int(split[2])] = float(split[3])
 
-print B[1]
+# Number of time frames 
+t = len(B)
 
+# Number of words in the lexicon
+N = len(lexicon)
+
+V = [{}]
+path = {}
+
+print 'Initialize Viterbi Trellis'
+for digit in lexicon.keys():
+	V[0][digit] = OrderedDict()
+	for i, phoneme in enumerate(lexicon[digit]):
+		if i == 0:
+			V[0][digit][phoneme] = (B[0][phoneme][0], 0, 0)
+		else:
+			V[0][digit][phoneme] = (0,0,0)
+
+print V[0]['seven']['S']
 
