cc303c115e7bf6f334bb82e9cd13de4d2032e097
pochuan <pochuan@stanford.edu>
2014-04-13 03:50:53 -0700

    going to sleep

--- viterbi.py
+++ viterbi.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 print "Viterbi!"
 
 # Read in files
-Lexicon = {}
+Lexicon = OrderedDict()
 with open('lexicon.txt') as f:
 	for line in f:
 		split = re.split(r'\s+', line)
@@ -33,29 +33,65 @@ T = len(B)
 # Number of words in the lexicon
 N = len(Lexicon)
 
+# List the states and keep track of properties of indexes
+states = []
+i = 0
+firstSubphone = [] # Idxes of the first subphone of a digit
+digitOfState = {} # dict of state idx to digit
+for digit, phonemeList in Lexicon.iteritems():
+	firstSubphone.append(i)
+	for phoneme in phonemeList:
+		for subphone in range(3):
+			states.append((i, digit, phoneme, subphone))
+			digitOfState[i] = digit
+			i = i + 1 
+
 V = [{}]
 path = {}
-
 print 'Initialize Viterbi Trellis'
-for digit in Lexicon.keys():
-	V[0][digit] = OrderedDict()
-	for i, phoneme in enumerate(Lexicon[digit]):
-		if i == 0:
-			V[0][digit][phoneme] = (B[0][phoneme][0], 0, 0)
-		else:
-			V[0][digit][phoneme] = (0,0,0)
-
-print V[0]['seven']
-
+for q in states:
+	i = q[0]
+	digit = q[1]
+	phoneme = q[2]
+	subphone = q[3]
+	if i in firstSubphone:
+		V[0][i] = B[0][phoneme][subphone]
+	else:
+		V[0][i] = 0
 
 print 'Run Viterbi for t > 0'
-for t in range(1, T):
+for t in range(T):
 	V.append({})
-	p = []
-	for digit, phonemes in Lexicon:
-		for phone in phonemes:
-			for subphone in range(3):
-				p.append(V[t-1][digit][phone][subphone]+B[t][phone][subphone])
+	for q in states:
+		i = q[0]
+		digit = q[1]
+		phoneme = q[2]
+		subphone = q[3]
+		
+
+# V = [{}]
+# path = {}
+
+# print 'Initialize Viterbi Trellis'
+# for digit in Lexicon.keys():
+# 	V[0][digit] = OrderedDict()
+# 	for i, phoneme in enumerate(Lexicon[digit]):
+# 		if i == 0:
+# 			V[0][digit][phoneme] = (B[0][phoneme][0], 0, 0)
+# 		else:
+# 			V[0][digit][phoneme] = (0,0,0)
+
+# print V[0]['seven']
+
+
+# print 'Run Viterbi for t > 0'
+# for t in range(1, T):
+# 	V.append({})
+# 	p = []
+# 	for digit, phonemes in Lexicon:
+# 		for phone in phonemes:
+# 			for subphone in range(3):
+# 				p.append(V[t-1][digit][phone][subphone]+B[t][phone][subphone])
 
 
 
