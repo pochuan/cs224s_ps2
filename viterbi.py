4c8f1365d62bb066d344294471f5e63b932cef82
pochuan <pochuan@stanford.edu>
2014-04-13 15:54:17 -0700

    Runs but is broken

--- viterbi.py
+++ viterbi.py
@@ -45,62 +45,53 @@ for digit, phonemeList in Lexicon.iteritems():
 			states.append((i, digit, phoneme, subphone))
 			digitOfState[i] = digit
 			i = i + 1 
-lastSubphone = firstSubphone[1:] - 1
-print firstSubphone
-print lastSubphone
 
-# V = [{}]
-# path = {}
-# print 'Initialize Viterbi Trellis'
-# for q in states:
-# 	i = q[0]
-# 	digit = q[1]
-# 	phoneme = q[2]
-# 	subphone = q[3]
-# 	if i in firstSubphone:
-# 		V[0][i] = B[0][phoneme][subphone]
-# 	else:
-# 		V[0][i] = 0
+lastSubphone = firstSubphone[1:]
+lastSubphone.append(i)
+lastSubphone = [x - 1 for x in lastSubphone]
 
-# print 'Run Viterbi for t > 0'
-# for t in range(T):
-# 	V.append({})
-# 	for q in states:
-# 		i = q[0]
-# 		digit = q[1]
-# 		phoneme = q[2]
-# 		subphone = q[3]
-# 		cases = []
-# 		# Stay in the same state
-# 		cases.append(V[t-1][i]+B[t][phone][subphone])
-# 		if i in firstSubphone:
-# 			# From last state of every digit
-# 			cases.append(max(V[t-1][ls]+B[t][phone][subphone] for ls in firstSubphone))
+V = [{}]
+path = {}
+print 'Initialize Viterbi Trellis'
+for q in states:
+	i = q[0]
+	digit = q[1]
+	phoneme = q[2]
+	subphone = q[3]
+	if i in firstSubphone:
+		V[0][i] = B[0][phoneme][subphone]
+	else:
+		V[0][i] = 0
+	path[i] = [i]
 
-# V = [{}]
-# path = {}
+print 'Run Viterbi for t > 0'
+for t in range(1,T):
+	V.append({})
+	newpath = {}
+	for q in states:
+		i = q[0]
+		digit = q[1]
+		phoneme = q[2]
+		subphone = q[3]
+		cases = []
+		# Stay in the same state
+		cases.append((V[t-1][i]+B[t][phoneme][subphone],i))
+		if i in firstSubphone:
+			# From last state of every digit (word transition)
+			cases.append((max(V[t-1][ls] + B[t][phoneme][subphone] - 50,ls )for ls in lastSubphone))
+		else:
+			# From the previous state in the same digit
+			cases.append((V[t-1][i-1] + B[t][phoneme][subphone],i-1))
+		(maxProb, maxState) = max(cases)
+		V[t][i] = maxProb
+		newpath[i] = path[maxState]+[i]
 
-# print 'Initialize Viterbi Trellis'
-# for digit in Lexicon.keys():
-# 	V[0][digit] = OrderedDict()
-# 	for i, phoneme in enumerate(Lexicon[digit]):
-# 		if i == 0:
-# 			V[0][digit][phoneme] = (B[0][phoneme][0], 0, 0)
-# 		else:
-# 			V[0][digit][phoneme] = (0,0,0)
+	# Don't need old paths
+	path = newpath
 
-# print V[0]['seven']
-
-
-# print 'Run Viterbi for t > 0'
-# for t in range(1, T):
-# 	V.append({})
-# 	p = []
-# 	for digit, phonemes in Lexicon:
-# 		for phone in phonemes:
-# 			for subphone in range(3):
-# 				p.append(V[t-1][digit][phone][subphone]+B[t][phone][subphone])
-
+(finalProb, finalState) = max((V[t][q[0]],q[0]) for q in states)
+print finalProb
+print path[finalState]
 
 
 
