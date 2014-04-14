69ff25c60e2a225b99d902d254a027865f02187e
pochuan <pochuan@stanford.edu>
2014-04-13 17:41:41 -0700

    It Works

--- viterbi.py
+++ viterbi.py
@@ -1,8 +1,6 @@
 import re
 from collections import OrderedDict
 
-print "Viterbi!"
-
 # Read in files
 Lexicon = OrderedDict()
 with open('lexicon.txt') as f:
@@ -29,6 +27,7 @@ with open('fake.output') as f:
 
 # Number of time frames 
 T = len(B)
+# T = 100
 
 # Number of words in the lexicon
 N = len(Lexicon)
@@ -61,7 +60,7 @@ for q in states:
 	if i in firstSubphone:
 		V[0][i] = B[0][phoneme][subphone]
 	else:
-		V[0][i] = 0
+		V[0][i] = float("-inf")
 	path[i] = [i]
 
 print 'Run Viterbi for t > 0'
@@ -74,20 +73,25 @@ for t in range(1,T):
 		phoneme = q[2]
 		subphone = q[3]
 		cases = []
+		stateObsL = B[t][phoneme][subphone]
 		# Stay in the same state
-		cases.append((V[t-1][i]+B[t][phoneme][subphone],i))
+		cases.append((V[t-1][i] + stateObsL, i))
+		
 		if i in firstSubphone:
 			# From last state of every digit (word transition)
-			cases.append((max(V[t-1][ls] + B[t][phoneme][subphone] - 50,ls)for ls in lastSubphone))
+			temp = max((V[t-1][ls] + stateObsL - 50, ls) for ls in lastSubphone)
+			cases.append(temp)
 		else:
 			# From the previous state in the same digit
-			cases.append((V[t-1][i-1] + B[t][phoneme][subphone],i-1))
+			cases.append((V[t-1][i-1] + stateObsL, i-1))
+		
 		(maxProb, maxState) = max(cases)
 		V[t][i] = maxProb
 		newpath[i] = path[maxState]+[i]
 
 	# Don't need old paths
 	path = newpath
+	# print newpath
 
 (finalProb, finalState) = max((V[t][i],i) for i in lastSubphone)
 finalPath = [path[finalState][0]]
@@ -95,16 +99,20 @@ for i in range(1, len(path[finalState])):
 	if path[finalState][i] != finalPath[-1]:
 		finalPath = finalPath + [path[finalState][i]]
 
-print finalPath
-
-
 output = []
 for i in finalPath:
 	if i in firstSubphone:
 		output.append(digitOfState[i])
-
+print finalPath
 print finalProb
 print output
 
+# # Print out trellis
+# s = "    " + " ".join(("%7d" % i) for i in range(len(V))) + "\n"
+# for y in V[0]:
+#     s += "%.5s: " % y
+#     s += " ".join("%.7s" % ("%f" % v[y]) for v in V)
+#     s += "\n"
+# print(s)
 
 
